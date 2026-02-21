bd90c6b1e8498f60a0518827ab356f763f8c2d59
mr4rahimi <mr4.rahimi@gmail.com>
2026-02-21 14:59:42 +0330

    add schema and gtm tags

---
+++ src/config/landing.ts
@@ -0,0 +1,7 @@
+export const LANDING = {
+  phone: "02191300348",
+  canonical: "https://bartar-repairer.com/landing-divar-laptop/",
+  brandName: "برتر سرویس",
+  city: "تهران",
+  blogUrl: "https://bartar-repairer.com/blog/",
+};
